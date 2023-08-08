a014e9b7d167ab7f551285ce66f2e0d0e175c273
Leonardo Koslowski <koslowskileonardo84@gmail.com>
2023-08-08 19:14:58 -0300

    Endpoint to return the posts from a user

--- src/main/java/com/leonardokoslowski/workshopmongo/resources/UserResource.java
+++ src/main/java/com/leonardokoslowski/workshopmongo/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.leonardokoslowski.workshopmongo.domain.Post;
 import com.leonardokoslowski.workshopmongo.domain.User;
 import com.leonardokoslowski.workshopmongo.dto.UserDTO;
 import com.leonardokoslowski.workshopmongo.services.UserService;
@@ -55,6 +56,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+	public ResponseEntity <List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 } 
 
